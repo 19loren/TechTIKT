62c201d296e10ff1413d64035d6693cb5860c7dc
Fernando Martins <fj407161@gmail.com>
2022-12-07 15:25:46 -0300

    Update Recarga
    Co-Authored-By: Loren Sev <80834658+19loren@users.noreply.github.com>
    Co-Authored-By: Henrique Ribeiro <77698071+HenriqueRibeiroo@users.noreply.github.com>
    Co-Authored-By: JulinBruto <104083214+JulinBruto@users.noreply.github.com>
    Co-Authored-By: luizall <104085042+luizall@users.noreply.github.com>

--- src/controllers/insertRecarga.js
+++ src/controllers/insertRecarga.js
@@ -2,5 +2,5 @@ require('./corinthians');
 
 module.exports = async function recarregar(codigo,tipoRecarga,saldoRecarga){
   await runQuery('UPDATE Bilhetes SET saldo_bilhete=:id where codigo_bilhete=:id',[saldoRecarga,codigo]);
-  await runQuery('insert into recarga values(:id, sysdate,:id,:id)',[codigo,tipoRecarga,codigo]);
+  await runQuery('insert into recarga values(sysdate,:id,:id)',[tipoRecarga,codigo]);
 }
